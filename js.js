c95fa23d79731910a2d4822ccf019bb8b0b86bc2
dvikster <dvikster@gmail.com>
2017-05-16 14:07:36 +0300

    upd

--- js.js
+++ js.js
@@ -2,20 +2,39 @@
  * Created by StudentGoIT on 16.05.2017.
  */
 var menuWrap = document.querySelector('.menu_wrap img');
+// var widthWrap = menuWrap.width;
+// console.log(widthWrap);
 console.log(menuWrap);
 
-menuWrap.onmousemove = function (event) {
-    // console.log(event);
-    if(event.clientX <300){
+menuWrap.addEventListener('mousemove', hoverMenu);
+// menuWrap.onmousemove = function (event) {
+function hoverMenu(event) {
+    console.log(event);
+    var widthWrap = event.toElement.clientWidth;
+    var widthPercent = (event.offsetX *100)/widthWrap;
+    if(widthPercent <= 2 || widthPercent >= 98){
+        menuWrap.src = 'img/0ru.png';
+    }
+    if(widthPercent >2 && widthPercent <33.3){
         menuWrap.src = 'img/1ru.png';
-        console.log('0-300');
+        console.log(widthPercent);
+        menuWrap.onclick = function () {
+            location.href = 'http://www.google.com';
+            // window.open('http://www.google.com','_blank');
+        }
     }
-    else if(event.clientX >=300 && event.clientX <=600) {
+    else if(widthPercent >=33.3 && widthPercent <=66.6) {
+        console.log(widthPercent);
         menuWrap.src = 'img/2ru.png';
-        console.log('300-600');
+        menuWrap.onclick = function () {
+            location.href = 'http://www.bing.com';
+        }
     }
-    else if(event.clientX >600) {
+    else if(widthPercent >66.6 && widthPercent <98) {
+        console.log(widthPercent);
         menuWrap.src = 'img/3ru.png';
-        console.log('600-900');
+        menuWrap.onclick = function () {
+            location.href = 'http://www.gmail.com';
+        }
     }
 }
